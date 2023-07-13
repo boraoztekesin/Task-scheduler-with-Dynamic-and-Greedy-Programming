8e2c6a6091aeba21d6912277cde825103262e197
boraoztekesin <123475096+boraoztekesin@users.noreply.github.com>
2023-07-14 00:52:36 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -29,14 +29,8 @@ public class Task implements Comparable {
     public boolean isUrgent() {
         return this.urgent;
     }
-
-    /**
-     * Finish time should be calculated here
-     *
-     * @return calculated finish time as String
-     */
+    
     public String getFinishTime() {
-        // YOUR CODE HERE
         int hour=Integer.parseInt(start.substring(0,2))+duration;
         //System.out.println(hour);
 
@@ -58,35 +52,15 @@ public class Task implements Comparable {
             }else {
                 return String.valueOf(hour)+":"+start.substring(start.length()-2);
             }
-
         }
-
-
-
     }
-
-
-    /**
-     * Weight calculation should be performed here
-     *
-     * @return calculated weight
-     */
     public double getWeight() {
         // YOUR CODE HERE
         return (double) importance*(urgent? 2000 : 1)/duration;
     }
 
-    /**
-     * This method is needed to use {@link java.util.Arrays#sort(Object[])} ()}, which sorts the given array easily
-     *
-     * @param o Object to compare to
-     * @return If self > object, return > 0 (e.g. 1)
-     * If self == object, return 0
-     * If self < object, return < 0 (e.g. -1)
-     */
     @Override
     public int compareTo(Object o) {
-        // YOUR CODE HERE
         if (o instanceof Task) {
             Task o1 = (Task) o;
             return this.getFinishTime().compareTo(o1.getFinishTime());
